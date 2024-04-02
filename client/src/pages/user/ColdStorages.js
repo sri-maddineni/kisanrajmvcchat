1e414e7e8a91a91f8c704985ebdd326b1ae06582
sri-maddineni <121371216+sri-maddineni@users.noreply.github.com>
2024-04-02 10:08:23 +0530

    done

--- client/src/pages/user/ColdStorages.js
+++ client/src/pages/user/ColdStorages.js
@@ -49,26 +49,22 @@ const ColdStorages = () => {
         <div className="r">
           {coldstorages.map((coldStorage) => (
             <div key={coldStorage._id}>
-<<<<<<< HEAD
+
               <div className="card" style={{ width: "18rem", minHeight:"18rem"}}>
-=======
+
               <div className="card" style={{ width: "18rem" }}>
->>>>>>> 0061845ab593420d72ab9c6b2ffc2f92c1eba656
+
                 <div className="card-img">
                   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZyvAhKAugumJj0TveFUEfRIpZ8qTeKf939w&usqp=CAU" style={{ width: "100px", objectFit: "cover" }} alt="cold" />
                 </div>
                 <div className="card-body">
                   <h5 className="card-title">{coldStorage.name}</h5>
-
                   <p className="card-text"><i className='fa-solid fa-phone'></i> {coldStorage.phone}</p>
                   <p className="card-text"><i className='fa-solid fa-location-dot'></i>  {coldStorage.address}</p>
                   <p className="card-text"><i class="fa-solid fa-warehouse"></i> {coldStorage.capacity ? coldStorage.capacity : "NA"} Metric tons</p>
-
-<<<<<<< HEAD
                 </div> 
-=======
                 </div>
->>>>>>> 0061845ab593420d72ab9c6b2ffc2f92c1eba656
+
               </div>
             </div>
           ))}
